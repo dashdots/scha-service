b69d0e0cb845b41ee3097326afd2ccbc46c939e7
dashdots <faculacola@gmail.com>
2016-11-08 22:38:15 +0800

    update executor entry

--- Executor.js
+++ Executor.js
@@ -1,12 +1,17 @@
 import ExecutorBase from './lib/ExecutorBase';
+import ExitCodes from './lib/ExitCodes';
 
 class Executor extends ExecutorBase {
 
   _parent;
   _childs;
+  _waitForDispose = false;
 
   constructor() {
     super({executorProcess: process});
+    process.stdin.resume();
+    process.on('SIGINT', ::this._onSIGINT);
+    process.on('SIGTERM', ::this._onSIGTERM);
   }
 
   get parent() {return this._parent;}
@@ -19,7 +24,36 @@ class Executor extends ExecutorBase {
   initialize() {
     super.identifier = identifier;
 
-  }}
+  }
+
+  exit({force=false}={}) {
+    if(force) {
+      this._forceExit();
+    } else {
+      this._cleanExit();
+    }
+  }
+
+  _onSIGINT() {
+    this.exit({force:this._waitForExit});
+  }
+
+  _onSIGTERM() {
+    this._forceExit({code: ExitCodes.FORCE_EXIT});
+  }
+
+  _forceExit() {
+    process.exit();
+  }
+
+  _cleanExit() {
+    if(this._waitForDispose) {
+      return;
+    }
+    process.exit();
+  }
+
+}
 
 
 export default Executor;
