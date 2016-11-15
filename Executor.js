825246695b15fb85a8515d757e66b9da8af0d3e5
dashdots <faculacola@gmail.com>
2016-11-15 19:43:47 +0800

    update executor entry

--- Executor.js
+++ Executor.js
@@ -1,6 +1,8 @@
 import ExecutorBase from './lib/ExecutorBase';
 import ExitCodes from './lib/ExitCodes';
 import TaskPool from './lib/TaskPool';
+import ExecutorEvent from './lib/ExecutorEvent';
+import Progress from './lib/Progress';
 
 class Executor extends ExecutorBase {
 
@@ -19,16 +21,23 @@ class Executor extends ExecutorBase {
     this._forceExit({code: ExitCodes.FORCE_EXIT});
   }
 
+  _onUncaughtException(err) {
+    this.emit({event:ExecutorEvent.error, data:err});
+    this._forceExit({code:ExitCodes.FATAL});
+  }
 
   constructor() {
     super({executorProcess: process});
     process.stdin.resume();
     process.on('SIGINT', ::this._onSIGINT);
     process.on('SIGTERM', ::this._onSIGTERM);
+    process.on('uncaughtException', ::this._onUncaughtException);
   }
 
   get parent() {return this._parent;}
   get childs() { return this._childs; }
+  get identifier() {return super.identifier;}
+  get idle() {return this._idle;}
 
   run() {
 
@@ -60,12 +69,23 @@ class Executor extends ExecutorBase {
   }
 
   _cleanExit() {
+
     if(this._waitForDispose) {
       return;
     }
+
+    if(this._childs && this._childs.count!==0) {
+      return;
+    }
+
     process.exit();
   }
 
+  get progress() {
+    return this._childs
+        ? Progress.merge(this._taskPool.progress, this._childs.progress)
+        : this._taskPool.progress;
+  }
 }
 
 
