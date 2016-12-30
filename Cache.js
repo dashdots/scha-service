1d2e404f19d3ca7dd5dad8230aa3382b999557e2
dashdots <faculacola@gmail.com>
2016-12-30 23:50:09 +0800

    update comment

--- Cache.js
+++ Cache.js
@@ -46,37 +46,101 @@ export default class Cache {
     return cacheDB(TEMP_DB);
   }
 
+  /**
+   * get dump db cmd instance
+   * @return {CacheDBCmd}
+   */
   static get dumpCmd() { return this.dumpDB.multi(); }
 
+  /**
+   * get data db cmd instance
+   * @return {CacheDBCmd}
+   */
   static get dataCmd() { return this.dataDB.multi(); }
 
+  /**
+   * get task db cmd instance
+   * @return {CacheDBCmd}
+   */
   static get taskCmd() { return this.taskDB.multi(); }
 
+  /**
+   * get temp db cmd instance
+   * @return {CacheDBCmd}
+   */
   static get tempCmd() { return this.tempDB.multi(); }
 
+  /**
+   * bulk run a cmd
+   * @param {CacheDB} db
+   * @param {CacheDBCmd} cmd
+   * @param {Function} cb
+   * @returns {Promise.<Object>}
+   * @private
+   */
   static async _runCmd(db, cmd, cb) {
     const execNow = !cmd;
     if(execNow) {
       cmd = db.multi();
     }
     await cb(cmd);
+    if(execNow) {
+      return await cmd.execAsync();
+    }
   }
+
+  /**
+   * bulk run a dump cmd
+   * @param {CacheDBCmd} cmd
+   * @param {Function} cb
+   * @returns {Promise.<Object>}
+   * @private
+   */
   static async runDumpCmd(cmd, cb) {
     return await this._runCmd(this.dumpDB, cmd, cb);
   }
 
+  /**
+   * bulk run a data cmd
+   * @param {CacheDBCmd} cmd
+   * @param {Function} cb
+   * @returns {Promise.<Object>}
+   * @private
+   */
   static async runDataCmd(cmd, cb) {
     return await this._runCmd(this.dataDB, cmd, cb);
   }
 
+  /**
+   * bulk run a task cmd
+   * @param {CacheDBCmd} cmd
+   * @param {Function} cb
+   * @returns {Promise.<Object>}
+   * @private
+   */
   static async runTaskCmd(cmd, cb) {
     return await this._runCmd(this.taskDB, cmd, cb);
   }
 
+  /**
+   * bulk run a proxy cmd
+   * @param {CacheDBCmd} cmd
+   * @param {Function} cb
+   * @returns {Promise.<Object>}
+   * @private
+   */
   static async runProxyCmd(cmd, cb) {
     return await this._runCmd(this.proxyDB, cmd, cb);
   }
 
+
+  /**
+   * bulk run a temp cmd
+   * @param {CacheDBCmd} cmd
+   * @param {Function} cb
+   * @returns {Promise.<Object>}
+   * @private
+   */
   static async runTempCmd(cmd, cb) {
     return await this._runCmd(this.tempDB, cmd, cb);
   }
