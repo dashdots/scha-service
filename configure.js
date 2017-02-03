edf5005a91d158cfb6b69d3ceda90d121ab96271
dashdots <faculacola@gmail.com>
2017-02-03 22:17:46 +0800

    add comment & update config

--- configure.js
+++ configure.js
@@ -3,6 +3,22 @@ export const TASK_DB = 4;
 export const DUMP_DB = 5;
 export const DATA_DB = 6;
 export const TEMP_DB = 7;
+
+export const HEAP_SNAPSHOT_REPORT_INTERVAL = 5 * 60 * 1000;
+
+export const CLEANUP_CHECK_INTERVAL = 1000;
+export const CLEANUP_TIMEOUT = 10 * 1000;
+export const CLEANUP_FORCE_TIMEOUT = CLEANUP_TIMEOUT + 60 * 1000;
+
+export const TASK_WATCHER_INTERVAL = 2*1000;
+
+export const WORKER_PROCESS_LIMIT = 20;
+export const WORKER_THREADS_LIMIT = 2;
+export const WORKER_TASK_LIMIT = 10;
+export const WORKER_RECYCLING_RUN_LOOP = 10;
+
+export const WORKER_RECYCLING_TIME = 2*60*1000;
+
 export const DUMP_PATH = '/data/dump';
 
 export const PROXY_RETRY_DELAY = 5000;
