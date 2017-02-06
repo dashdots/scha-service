1d6969c3e5fa67c4a28cc711f9c8e0815d44cefc
dashdots <faculacola@gmail.com>
2017-02-06 22:17:14 +0800

    update leecher

--- Leecher.js
+++ Leecher.js
@@ -19,4 +19,67 @@ export const LeecherEvent = {
 
 export default class Leecher extends EventEmitter{
 
+
+  _proxy;
+  _leechType;
+  _siteName;
+  _listOnly;
+  _cookie;
+  _timeout;
+  _protocol;
+  _hostName;
+  _leechStoreKey;
+  _dumpIndexKey;
+  _dumpKey;
+
+
+  get proxy() {return this._proxy;}
+  get leechType() {return this._leechType;}
+  get siteName() {return this._siteName;}
+  get listOnly() {return this._listOnly;}
+  get cookie() {return this._cookie;}
+  get timeout() {return this._timeout;}
+  get protocol() {return this._protocol;}
+  get hostName() {return this._hostName;}
+
+  get leechStoreKey() {return this._leechStoreKey; }
+  get dumpIndexKey(){ return this._dumpIndexKey; }
+  get dumpKey(){ return this._dumpKey; }
+
+  constructor(siteName) {
+    super();
+    const leecher = require(`scha.dump.sites/lib/${siteName}`);
+
+    let {
+        HOST_NAME,
+        PROTOCOL,
+        TIMEOUT,
+        COOKIE,
+        LEECH_TYPE,
+        LIST_ONLY,
+        DOM_DECODE_ENTITIES=false,
+        LANG,
+
+    } = leecher;
+
+
+    const NAME = siteName;
+    this._siteName = NAME;
+    this._lang = LANG;
+    this._homeUrl = PROTOCOL+'://'+HOST_NAME.replace(/\/+$/,'')+'/';
+    this._hostName = HOST_NAME;
+    this._protocol = PROTOCOL;
+    this._timeout = TIMEOUT;
+    this._cookie = COOKIE;
+    this._leechType = LEECH_TYPE;
+    this._listOnly = LIST_ONLY;
+    this._domDecodeEntities = DOM_DECODE_ENTITIES;
+  }
+
+  async getListCount() {
+    const db = Cache.dumpDB;
+    //noinspection JSUnresolvedFunction
+    const totalPageCount = await db.zcardAsync(this._dumpIndexKey);
+    return Math.ceil(totalPageCount / this._listLinkCount);
+  }
 }
