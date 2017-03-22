82f2eba246bec38822a58a266d84b1094d828ac6
dashdots <faculacola@gmail.com>
2017-03-22 10:05:58 +0800

    update list leech task

--- tasks/ListLeechTask.js
+++ tasks/ListLeechTask.js
@@ -23,14 +23,46 @@ class ListLeechTask extends Task {
     });
 
     this.leecher = new Leecher(name);
+    this.leecher.on(LeecherEvent.leechLeak, msg=>{
+      this.log.attention(msg);
+    });
+    this.leecher.on(LeecherEvent.message, msg=>{
+      this.log.trace(msg);
+    });
+    this.leecher.proxy.on(ProxyDistributorEvent.notice, msg=>{
+      this.log.trace(`fetch ${page}: ${msg}`);
+    });
+    this.leecher.proxy.on(ProxyDistributorEvent.warning, msg=>{
+      this.log.warn(`fetch ${page}: ${msg}`);
+    });
+
+
+    this.on(Task.Events.success, ()=>{
+      this.log.success(`leech ${page} success`);
+    });
+    this.on(Task.Events.failed, (sender, reason)=>{
+      this.log.error(reason, `leech ${page} failed`);
+    });
+    this.on(Task.Events.complete,()=>{
+      this.log.removeAllListeners('log');
+    });
   }
 
   async run() {
     const {force, ignoreNext, page, loadDumpCache, loadDumpFile, rewriteDump} = this.fields;
     const log = this.log;
+
     let results = [];
+
     log.notice(`run task ${page} ${force?'force':''}`);
-    results = await this.leecher.fetchListResult({page, dumpPath:DUMP_PATH, loadDumpCache, loadDumpFile, rewriteDump, force});
+    try {
+      results = await this.leecher.fetchListResult({page, dumpPath:DUMP_PATH, loadDumpCache, loadDumpFile, rewriteDump, force});
+    } catch(e) {
+      console.log(e);
+      log.error(`BUG: ${page}, ${e.message}`);
+      return this.failed();
+    }
+
     return this.success();
   }
 }
