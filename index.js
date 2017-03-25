5f5a36be4aea8c5bc4935b7d4f3187fa63d0034c
dashdots <faculacola@gmail.com>
2017-03-25 23:47:21 +0800

    update entry

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import 'source-map-support/register';
 import 'babel-polyfill';
 import Log from 'scha.lib/lib/Log';
 import executor, {ExecutorEvent, Task, TaskFactory, TaskType} from './Executor';
+import Leecher from './Leecher';
+import LeechTaskType from './LeecherTaskType';
 
 const log = new Log({name:'Leecher', logName:false, showPid:true});
 
@@ -22,4 +24,21 @@ const taskFactory = new NopTaskFactory();
 
 function onPeerMessage(sender, {event, data}) {
   log.debug({event, data}, 'peer message:');
+}
+
+
+async function buildPageTasks({name, begin=0, end, tasks, ...params}) {
+  name = name.toUpperCase();
+  if(!tasks || !tasks.length) {
+    tasks = await (new Leecher(name)).loadPageIdsByRange(begin, end) || [];
+  }
+  return tasks.map(pageId=>Object.assign({}, params, {name, pageId}));
+}
+
+async function buildSyncholizeTasks({name, begin = 0, end, tasks, ...params}) {
+  name = name.toUpperCase();
+  if(!tasks || !tasks.length) {
+    tasks = await (new Leecher(name)).loadPageIdsByRange(begin, end) || [];
+  }
+  return tasks.map(pageId=>Object.assign({}, params, {name, pageId}));
 }
