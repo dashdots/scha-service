cc7969b5cb7c4721f60453bae07cd03184bb3353
dashdots <faculacola@gmail.com>
2017-04-02 20:20:29 +0800

    clean up & add comment

--- SiteLeecherLoader.js
+++ SiteLeecherLoader.js
@@ -1,5 +1,19 @@
-import {BannedError, InvalidError} from 'scha.dump.proxy';
+/**
+ * Leecher script loader
+ */
 
+import {BannedError, InvalidError} from 'scha.dump.proxy';
+import stringHash from 'string-hash';
+import BigNumber from 'bignumber.js';
+import LeecherCache from './LeecherCache';
+import parseDOM from 'scha.lib/lib/DOM';
+
+// region tools
+/**
+ * Wrap regexp or function to validator
+ * @param {RegExp|Function} validator
+ * @returns {Function|Boolean}
+ */
 function _wrapValidator(validator) {
   if(validator instanceof RegExp) {
     return function(text) { return validator.test(text); }
@@ -9,6 +23,13 @@ function _wrapValidator(validator) {
   return false;
 }
 
+/**
+ * Make a resource converter
+ * @param {Function} listResConverter
+ * @param {RegExp|Function} bannedValidator
+ * @param {RegExp|Function} contentValidator
+ * @returns {Function}
+ */
 function _makeResConverter(listResConverter=x=>x.text(), bannedValidator=/your ip /i, contentValidator=x=>x && x.length>1024*5) {
 
   bannedValidator = _wrapValidator(bannedValidator);
@@ -26,6 +47,12 @@ function _makeResConverter(listResConverter=x=>x.text(), bannedValidator=/your i
   };
 }
 
+/**
+ * Make http headers parser
+ * @param {Object} headersParser - an object container header key-value pairs
+ * @param {String} COOKIE - set cookie string in header
+ * @returns {function(*=)}
+ */
 function _makeHeadersParser(headersParser={}, COOKIE) {
   return page => {
     let headers = headersParser;
@@ -36,70 +63,45 @@ function _makeHeadersParser(headersParser={}, COOKIE) {
   };
 }
 
+/**
+ * load leecher scripts
+ * @param {String} siteName
+ * @returns {*}
+ */
 function _loadLeecher(siteName) {
   const leechModule = require(`scha.dump.sites/lib/${siteName}`);
   return leechModule;
 }
 
+/**
+ * Generate hash key
+ * @param {String} name
+ * @param {String} key
+ * @returns {string}
+ */
 function _hashKey(name, key) {
   let hash = stringHash(name+'|'+key);
   hash = new BigNumber(hash, 10);
   return hash.toString(62);
 }
 
+/**
+ * Generate hash resource key
+ * @param {String} name
+ * @param {String} resource
+ * @returns {string}
+ * @private
+ */
 function _hashResource(name, resource){
   return _hashKey(name, resource.url);
 }
+// endregion
 
-export function listLeecherLoader(siteName) {
-  const leecher = _loadLeecher(siteName);
-
-  let {
-      HOST_NAME, PROTOCOL, TIMEOUT, COOKIE, LEECH_TYPE, LIST_LANG=['en'], LIST_LINK_COUNT, LIST_CONTENT_TYPE='HTML',
-      LIST_DOM_SELECTOR: DOM_SELECTOR,
-      LIST_DOM_REMOVAL_SELECTOR: DOM_REMOVAL_SELECTOR,
-      LIST_ONLY=false,
-      DOM_DECODE_ENTITIES=false,
-
-      listDomModifier: domModifier,
-      bannedValidator,
-      listValidator,
-      listHeadersParser,
-      listResConverter,
-      getListUrl,
-      parseListItem: parse,
-  } = leecher;
-  LEECH_TYPE = LEECH_TYPE.toUpperCase();
-
-  const HOME_URL = PROTOCOL+'://'+HOST_NAME.replace(/\/+$/,'')+'/';
-  const resConverter = _makeResConverter(listResConverter, bannedValidator, listValidator);
-  const headersParser = _makeHeadersParser(listHeadersParser, COOKIE);
-  const getUrl = list => getListUrl(list).replace(/^\/+/, HOME_URL);
-  const getSign = list => `${LEECH_TYPE}:${siteName}:${list}`;
-  const NAME = siteName.toUpperCase();
-
-  if(Array.isArray(DOM_REMOVAL_SELECTOR)) {
-    DOM_REMOVAL_SELECTOR = DOM_REMOVAL_SELECTOR.join(',');
-  }
-
-  return {
-    HOST_NAME, PROTOCOL, TIMEOUT, COOKIE, LEECH_TYPE, LANG:LIST_LANG, HOME_URL, LIST_LINK_COUNT, NAME, CONTENT_TYPE: LIST_CONTENT_TYPE,
-    DOM_SELECTOR,
-    DOM_REMOVAL_SELECTOR,
-    LIST_ONLY,
-    DOM_DECODE_ENTITIES,
-
-    domModifier,
-    resConverter,
-    headersParser,
-    getUrl,
-    getSign,
-    parse,
-    _hashResource:x=>_hashResource(NAME,x),
-    _hashKey:x=>_hashKey(NAME,x),
-  }
-}
-
+/**
+ * Load page leecher
+ * @param {String} siteName
+ * @returns {Object}
+ */
 export function pageLeecherLoader(siteName) {
   const leecher = _loadLeecher(siteName);
 
@@ -201,5 +203,59 @@ export function pageLeecherLoader(siteName) {
     _hashResource:x=>_hashResource(NAME,x),
     _hashKey:x=>_hashKey(NAME,x),
     cache,
+  }
+}
+
+/**
+ * Load list leecher
+ * @param {String} siteName
+ * @returns {Object}
+ */
+export function listLeecherLoader(siteName) {
+  const leecher = _loadLeecher(siteName);
+
+  let {
+      HOST_NAME, PROTOCOL, TIMEOUT, COOKIE, LEECH_TYPE, LIST_LANG=['en'], LIST_LINK_COUNT, LIST_CONTENT_TYPE='HTML',
+      LIST_DOM_SELECTOR: DOM_SELECTOR,
+      LIST_DOM_REMOVAL_SELECTOR: DOM_REMOVAL_SELECTOR,
+      LIST_ONLY=false,
+      DOM_DECODE_ENTITIES=false,
+
+      listDomModifier: domModifier,
+      bannedValidator,
+      listValidator,
+      listHeadersParser,
+      listResConverter,
+      getListUrl,
+      parseListItem: parse,
+  } = leecher;
+  LEECH_TYPE = LEECH_TYPE.toUpperCase();
+
+  const HOME_URL = PROTOCOL+'://'+HOST_NAME.replace(/\/+$/,'')+'/';
+  const resConverter = _makeResConverter(listResConverter, bannedValidator, listValidator);
+  const headersParser = _makeHeadersParser(listHeadersParser, COOKIE);
+  const getUrl = list => getListUrl(list).replace(/^\/+/, HOME_URL);
+  const getSign = list => `${LEECH_TYPE}:${siteName}:${list}`;
+  const NAME = siteName.toUpperCase();
+
+  if(Array.isArray(DOM_REMOVAL_SELECTOR)) {
+    DOM_REMOVAL_SELECTOR = DOM_REMOVAL_SELECTOR.join(',');
+  }
+
+  return {
+    HOST_NAME, PROTOCOL, TIMEOUT, COOKIE, LEECH_TYPE, LANG:LIST_LANG, HOME_URL, LIST_LINK_COUNT, NAME, CONTENT_TYPE: LIST_CONTENT_TYPE,
+    DOM_SELECTOR,
+    DOM_REMOVAL_SELECTOR,
+    LIST_ONLY,
+    DOM_DECODE_ENTITIES,
+
+    domModifier,
+    resConverter,
+    headersParser,
+    getUrl,
+    getSign,
+    parse,
+    _hashResource:x=>_hashResource(NAME,x),
+    _hashKey:x=>_hashKey(NAME,x),
   }
 }
